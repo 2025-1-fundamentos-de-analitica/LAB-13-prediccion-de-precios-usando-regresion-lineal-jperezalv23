cb955f5b52d715231f2ea73f5a7d4398b7fdf069
juanp <jperezalv@unal.edu.co>
2025-07-16 00:44:10 -0500

    solution

--- homework/homework.py
+++ homework/homework.py
@@ -61,3 +61,94 @@
 # {'type': 'metrics', 'dataset': 'train', 'r2': 0.8, 'mse': 0.7, 'mad': 0.9}
 # {'type': 'metrics', 'dataset': 'test', 'r2': 0.7, 'mse': 0.6, 'mad': 0.8}
 #
+import os
+import json
+import gzip
+import pickle
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
+from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score, mean_squared_error, median_absolute_error
+
+
+# Cargar datos
+df_entreno = pd.read_csv("./files/input/train_data.csv.zip", index_col=False, compression="zip")
+df_test = pd.read_csv("./files/input/test_data.csv.zip", index_col=False, compression="zip")
+
+df_entreno["Age"] = 2021 - df_entreno["Year"]
+df_test["Age"] = 2021 - df_test["Year"]
+
+df_entreno.drop(columns=["Year", "Car_Name"], inplace=True)
+df_test.drop(columns=["Year", "Car_Name"], inplace=True)
+
+X_train = df_entreno.drop(columns=["Present_Price"])
+y_train = df_entreno["Present_Price"]
+
+X_test = df_test.drop(columns=["Present_Price"])
+y_test = df_test["Present_Price"]
+
+columnas_categoricas = ["Fuel_Type", "Selling_type", "Transmission"]
+columnas_numericas = [col for col in X_train.columns if col not in columnas_categoricas]
+
+preprocesado = ColumnTransformer([
+    ("cat", OneHotEncoder(), columnas_categoricas),
+    ("scaler", MinMaxScaler(), columnas_numericas)
+])
+
+pipeline_modelo = Pipeline([
+    ("preprocessor", preprocesado),
+    ("feature_selection", SelectKBest(f_regression)),
+    ("classifier", LinearRegression())
+])
+
+param_grid = {
+    "feature_selection__k": range(1, 12),
+    "classifier__fit_intercept": [True, False],
+    "classifier__positive": [True, False],
+}
+
+buscador = GridSearchCV(
+    estimator=pipeline_modelo,
+    param_grid=param_grid,
+    cv=10,
+    scoring="neg_mean_absolute_error",
+    n_jobs=-1,
+    refit=True,
+    verbose=1,
+)
+
+buscador.fit(X_train, y_train)
+
+# Evaluación
+pred_entreno = buscador.predict(X_train)
+pred_test = buscador.predict(X_test)
+
+metricas_entreno = {
+    "type": "metrics",
+    "dataset": "train",
+    "r2": r2_score(y_train, pred_entreno),
+    "mse": mean_squared_error(y_train, pred_entreno),
+    "mad": median_absolute_error(y_train, pred_entreno),
+}
+
+metricas_test = {
+    "type": "metrics",
+    "dataset": "test",
+    "r2": r2_score(y_test, pred_test),
+    "mse": mean_squared_error(y_test, pred_test),
+    "mad": median_absolute_error(y_test, pred_test),
+}
+
+os.makedirs("files/output/", exist_ok=True)
+with open("files/output/metrics.json", "w") as archivo_salida_metricas:
+    archivo_salida_metricas.write(json.dumps(metricas_entreno) + "\n")
+    archivo_salida_metricas.write(json.dumps(metricas_test) + "\n")
+
+# Guardar modelo entrenado
+os.makedirs("files/models/", exist_ok=True)
+with gzip.open("files/models/model.pkl.gz", "wb") as archivo_salida:
+    pickle.dump(buscador, archivo_salida)
